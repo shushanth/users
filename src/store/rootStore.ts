1b41384942909d5accc3121e634327dde55afecd
shushanth <shushant3112@gmail.com>
2023-12-30 21:34:31 +0100

    Initalised home component with routes

--- src/store/rootStore.ts
+++ src/store/rootStore.ts
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import appSlice from "./appSlice";
 
 const rootStore = configureStore({
-  reducer: {},
+  reducer: {
+    app: appSlice,
+  },
+  devTools: process.env.NODE_ENV !== "production",
 });
 
 export type RootState = ReturnType<typeof rootStore.getState>;
